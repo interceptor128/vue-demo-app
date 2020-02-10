822fdee936dee6e84bec922101f978cc32d35440
interceptor128 <polestar.interceptor.0128@gmail.com>
2020-02-10 17:32:12 +0900

    V-Calendar導入

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Login from '../views/Login.vue'
+import Calendar from '../views/Calendar.vue'
 
 Vue.use(VueRouter)
 
@@ -15,6 +16,11 @@ const routes = [
     path: '/Home',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/Calendar',
+    name: 'Calendar',
+    component: Calendar
   }
 ]
 
