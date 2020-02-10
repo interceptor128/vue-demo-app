04864004991b3bdc4040363a5c15aec8d61915ce
interceptor128 <polestar.interceptor.0128@gmail.com>
2020-02-10 17:44:05 +0900

    calendar view edit

--- src/views/Calendar.vue
+++ src/views/Calendar.vue
@@ -1,3 +1,22 @@
 <template>
-    <v-calendar></v-calendar>
+    <v-app id="inspire">
+        <v-content>
+            <v-container
+            class="fill-height"
+            fluid
+            >
+                <v-row
+                align="center"
+                justify="center"
+                >
+                    <v-calendar></v-calendar>
+                </v-row>
+            </v-container>
+        </v-content>
+        <v-content>
+            <v-date-picker :mode="mode"
+               :formats="formats"
+               v-model="selectedDate"></v-date-picker>
+        </v-content>
+    </v-app>
 </template>
